f8924ad26a76461fc072e78b4f85eb538007d45d
Lindrit <lindritsulaj2.0@gmail.com>
2023-07-16 00:45:10 +0200

    Get users api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Post, Topics } from "@/components"
-import { getServerSession } from 'next-auth/next'
-import { options } from "./api/auth/[...nextauth]/options";
-import prisma from "@/lib/prisma";
+
+export async function getUsers() {
+  const res = await fetch('http://localhost:3000/api/users');
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  };
+
+  return res.json();
+}
 
 export default async function Home() {
+  const users = await getUsers();
+
   return (
     <main className="mt-[67px] w-full h-screen bg-neutral-100">
       <Post />
       <Topics />
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </main>
   )
 }
