90cd090e4e482215f50d8244d2e48a9fdd01f54f
Lindrit <lindritsulaj2.0@gmail.com>
2023-07-16 12:13:57 +0200

    Authentication completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -89,9 +89,17 @@ export default function Navbar() {
             </>
           )}
           {status === "authenticated" && (
-            <pre>
-              {JSON.stringify(data, null, 2)}
-            </pre>
+            <div className='flex items-center gap-3'>
+              <Link href="/profile" className='flex justify-center items-center gap-1'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.6} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <span className='text-lg'>Profile</span>
+              </Link>
+              <button onClick={() => signOut()} className="mx-auto px-6 border-solid border-[1px] border-neutral-300 hover:bg-neutral-100 rounded-md hover:underline py-[6px]">
+                Log out
+              </button>
+            </div>
           )}
         </div>
       </div>
