2698eaecf9ef2d5d13ba4abcf2f9b5f867d6a8f4
Lindrit <lindritsulaj2.0@gmail.com>
2023-07-17 11:33:03 +0200

    Sign out

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
 import { Post, Topics } from "@/components"
 import prisma from "@/lib/prisma";
 
-async function getUsers() {
-  const users = await prisma.user.findMany();
-  return users;
-}
-
 export default async function Home() {
-  const users = await getUsers();
-
   return (
     <main className="mt-[67px] w-full h-screen bg-neutral-100">
       <Post />
       <Topics />
-      <pre>{JSON.stringify(users, null, 2)}</pre>
     </main>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -54,6 +54,9 @@ export default function Navbar() {
                   </svg>
                   <span className='text-lg'>Profile</span>
                 </Link>
+                <Link href="/new" className='text-blue-600 font-medium border-solid border-[1px] border-blue-600 px-3 py-[7px] rounded-md hover:underline hover:text-white hover:bg-blue-600 mx-auto block my-2'>
+                  Create Post
+                </Link>
                 <button onClick={() => signOut()} className="mx-auto px-6 bg-blue-500 text-white rounded-md hover:bg-blue-600 hover:underline py-[6px] mt-3">
                   Log out
                 </button>
@@ -98,6 +101,11 @@ export default function Navbar() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
               </Link>
+              <button onClick={() => signOut()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                </svg>
+              </button>
             </div>
           )}
         </div>
